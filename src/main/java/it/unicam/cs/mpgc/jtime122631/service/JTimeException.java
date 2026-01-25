3e8b3407c2c38031cec4ecd522be3b1ea9cfff1f
Nicola Capancioni <nicolacapancioni@gmail.com>
2026-01-25 12:47:38 +0100

    add custom Runtime & SQL Exception  class

---
+++ src/main/java/it/unicam/cs/mpgc/jtime122631/service/JTimeException.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.jtime122631.service;
+
+public class JTimeException extends RuntimeException {
+    public JTimeException(String message) {
+        super(message);
+    }
+}
